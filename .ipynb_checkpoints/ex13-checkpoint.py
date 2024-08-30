605024f8a6d4d3bf5338591a59bf0a00055fdaf3
ryan <ryankaneohe@gmail.com>
2024-08-30 07:53:17 -0400

    initial commit of 'dot' files like gitignore and ipynb checkpoints

---
+++ .ipynb_checkpoints/ex13-checkpoint.py
@@ -0,0 +1,13 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+first = input("What's the first item?")
+second = input("What's the second item?")
+third = input("What's the third item?")
+
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
