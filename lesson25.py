e8d3133904e3e6417ca599de6bb3828c21d7cfea
ryan <ryankaneohe@gmail.com>
2024-09-27 08:41:01 -0400

    finished ex 29

---
+++ lesson25.py
@@ -0,0 +1,22 @@
+# Study Drill
+#
+# You now have a nice piece of code that's controlling a car.
+# In this study drill you're going to create a new function that creates any car.
+# Your creator function should meet these requirements:
+#
+# It should take parameters to set things like the color, speed, or anything else
+#   your cars can do.
+# It should create a dict that has the correct settings, and already contains all
+#   the functions you've created.
+# It should return this dict so people can assign the results to anything they want
+#   and use later.
+# It should be written so that someone can create any number of different cars and
+#   each one they make is independent.
+#
+# Your code should test #4 by changing settings in a few different cars and then
+# confirming they didn't change in other cars.
+#
+# This challenge is different because I'll show you the answer to the challenge in
+# a later exercise. If you struggle with this challenge then shelve it for a bit and
+# move on. You'll see this again shortly.
+
